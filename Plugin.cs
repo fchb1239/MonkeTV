505c6e69f55978d2b655aefa36007aa0938df13d
fchb1239 <fchb1239.dk@gmail.com>
2022-04-23 10:51:05 +0200

    totally optimised src

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using BepInEx;
+using Bepinject;
+using System.Reflection;
+using Utilla;
+using System.ComponentModel;
+
+namespace MonkeTV
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            var harmony = new Harmony(PluginInfo.GUID);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
