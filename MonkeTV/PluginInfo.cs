dc8035bdcd703084c11739ce723101d24329eeb6
dev9998 <81720436+developer9998@users.noreply.github.com>
2022-12-28 18:33:22 -0330

    Update PluginInfo.cs
    - Bumped version to 1.1.0 from 1.0.1
    - Updated mod name

--- MonkeTV/PluginInfo.cs
+++ MonkeTV/PluginInfo.cs
@@ -3,7 +3,7 @@ namespace MonkeTV
     class PluginInfo
     {
         public const string GUID = "org.fchb1239.gorillatag.monketv";
-        public const string Name = "Monke TV";
-        public const string Version = "1.0.1";
+        public const string Name = "MonkeTV";
+        public const string Version = "1.1.0";
     }
 }
